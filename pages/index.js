42a94012d0ced38c61ef3c7834a0d7f62695d2fe
Shadaj Tiwari <shadajtiwari01@gmail.com>
2021-12-26 11:39:13 +0530

    e

--- pages/index.js
+++ pages/index.js
@@ -187,19 +187,13 @@ const Home = () => {
               </div>
             </footer>
 
-            <BottomNavigation
-  showLabels
-  value={value}
-  onChange={(event, newValue) => {
-    setValue(newValue);
-  }}
->
-  <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-  <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-  <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-</BottomNavigation>
-
-
+            <BottomNavigation showLabels value={value} onChange={(event, newValue) => {
+              setValue(newValue);
+            }}>
+              <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+              <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+              <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+            </BottomNavigation>
 
     </div>
   )
