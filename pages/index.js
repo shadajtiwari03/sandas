24baf15bd25fccc8f6454cb2ccd7aa88007b1a0b
Shadaj Tiwari <shadajtiwari01@gmail.com>
2021-12-26 11:23:59 +0530

    Material Ui Updated

--- pages/index.js
+++ pages/index.js
@@ -187,6 +187,17 @@ const Home = () => {
               </div>
             </footer>
 
+            <BottomNavigation
+  showLabels
+  value={value}
+  onChange={(event, newValue) => {
+    setValue(newValue);
+  }}
+>
+  <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+  <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+  <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+</BottomNavigation>
 
 
 
